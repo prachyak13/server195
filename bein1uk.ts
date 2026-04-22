4169a8593b7ab4605e7c57f132a820a831464336
prachyak13 <prachya.g13@gmail.com>
2026-04-22 15:25:07 +0700

    Update bein1uk.ts

--- bein1uk.ts
+++ bein1uk.ts
@@ -2,5 +2,5 @@
 #EXT-X-VERSION:3
 #EXT-X-ALLOW-CACHE:YES
 
-#EXT-X-STREAM-INF:BANDWIDTH=2200000,NAME="1"
+#EXTINF:-1,ช่องหมายเลข 1
 https://starshare.net:443/live/8c:ea:48:d4:ee:c2/8c:ea:48:d4:ee:c20/316.ts
